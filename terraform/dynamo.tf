53ef645fcee1b49a0707ce33f14e0be78ef924ca
Maria Tzanidaki <maria.tzanidaki@mdlz.com>
2024-11-01 20:16:55 +0200

    updated patches
    Signed-off-by: Maria Tzanidaki <maria.tzanidaki@mdlz.com>

--- terraform/dynamo.tf
+++ terraform/dynamo.tf
@@ -1,6 +1,6 @@
 module "dynamodb_table" {
   source  = "terraform-aws-modules/dynamodb-table/aws"
-  version = "~>3.1.2"
+  version = "~>3.1"
 
   name     = "dynamo-table"
   hash_key = "user_id"
@@ -25,7 +25,7 @@ module "dynamodb_table" {
 
 module "delete_dynamodb_table" {
   source  = "terraform-aws-modules/dynamodb-table/aws"
-  version = "~>3.1.2"
+  version = "~>3.1"
 
   name     = "delete-dynamo-table"
   hash_key = "user_id"
